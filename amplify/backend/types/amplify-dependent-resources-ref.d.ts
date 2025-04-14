68592ce9a1c48abc2617df8cf8b1e95e9c7c5cf5
bll <iea.billy.kd@gmail.com>
2025-04-14 11:38:39 +0200

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "function": {
-    "iwiddevopsrbba3c46e55": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
