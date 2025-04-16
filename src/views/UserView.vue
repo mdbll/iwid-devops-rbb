bf55292dad20e08f286037985fef48ed8de0dfb8
Ryan Ez Zerqti <ezzerqtiryan@gmail.com>
2025-04-16 11:29:08 +0200

    add getUSer

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -8,32 +8,22 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { get } from "aws-amplify/api";
-import { useSignUpStore } from "@/stores/signUpStore";
 import handleSignOut from "@/functions/auth/handleSignOut";
 
-const { data } = useSignUpStore();
-console.log(data);
-
-// Déclaration de la donnée reçue de l'API
 const userData = ref(null);
 
-// Appel API à /getUser au montage du composant
 onMounted(async () => {
   const getUser = async () => {
     try {
       const restOperation = get({
         apiName: "auth",
-        path: "/getUser-ryan",
+        path: "/getUser",
       });
-      console.log({ restOperation });
       const res = await restOperation.response;
-      console.log({ res });
       const response = await res.body.json();
-
-      console.log("GET call succeeded");
-      console.log(response);
+      console.log({ response });
     } catch (e) {
-      console.log("POST call failed: ", JSON.parse(e.response.body));
+      console.log("GET call failed: ", { e });
     }
   };
   await getUser();
