bea8d6a51446dc0e1866577853e9b70fdf05f74d
bll <iea.billy.kd@gmail.com>
2025-04-17 15:22:49 +0200

    add edouard front

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -1,68 +1,62 @@
-<script>
-import { useForm } from "vue-hooks-form";
-import handleSignIn from "@/functions/auth/SignIn";
-
-export default {
-  setup() {
-    const { useField, handleSubmit } = useForm({
-      defaultValues: {},
-    });
-    const username = useField("username", {
-      rule: {
-        required: true,
-      },
-    });
-
-    const password = useField("password", {
-      rule: {
-        required: true,
-      },
-    });
-
-    const onSubmit = async (data) => {
-      console.log(data);
-      await handleSignIn(data);
-    };
-    return {
-      username,
-      password,
-      onSubmit: handleSubmit(onSubmit),
-    };
-  },
-};
-</script>
-
 <template>
-  <h1>Sign in</h1>
-  <form @submit="onSubmit" class="form">
-    <input
-      v-model="username.value"
-      :ref="username.ref"
-      class="input"
-      placeholder="username"
-    />
-    <p v-if="username.error">{{ username.error.message }}</p>
-    <input
-      v-model="password.value"
-      :ref="password.ref"
-      type="password"
-      class="input"
-      placeholder="Password"
-    />
-    <p v-if="password.error">{{ password.error.message }}</p>
-    <button type="submit">submit</button>
-  </form>
+  <div
+      class="px-6 h-screen py-20 md:px-12 lg:px-20 flex items-center justify-center bg-[linear-gradient(-225deg,var(--p-primary-500),var(--p-primary-700)_48%,var(--p-primary-800))] dark:bg-[linear-gradient(-225deg,var(--p-primary-400),var(--p-primary-600)_48%,var(--p-primary-800))]"
+  >
+      <div class="p-12 shadow text-center lg:w-[30rem] backdrop-blur-md rounded-xl bg-[rgba(255,255,255,0.1)]">
+          <div class="text-4xl font-medium mb-12 text-primary-contrast">Bienvenue</div>
+          <InputText
+              type="email"
+              class="!appearance-none placeholder:!text-primary-contrast/40 !border-0 !p-4 !w-full !outline-0 !text-xl !block !mb-6 !bg-white/10 !text-primary-contrast/70 !rounded-full"
+              placeholder="Email"
+              required
+              v-model="email"
+          />
+          
+          <InputText
+              type="password"
+              class="!appearance-none placeholder:!text-primary-contrast/40 !border-0 !p-4 !w-full !outline-0 !text-xl !mb-6 !bg-white/10 !text-primary-contrast/70 !rounded-full"
+              placeholder="Password"
+              required
+              v-model="password"
+          />
+          <button
+          @click="onSubmit"
+              type="button"
+              class="max-w-40 w-full rounded-full appearance-none border-0 p-4 outline-0 text-xl mb-6 font-medium bg-white/30 hover:bg-white/40 active:bg-white/20 text-primary-contrast/80 cursor-pointer transition-colors duration-150"
+          >
+              Sign In
+          </button>
+          <a class="cursor-pointer font-medium block text-center text-primary-contrast" @click="goToRegister">Register</a>
+      </div>
+  </div>
 </template>
 
-<style>
-.input {
-  padding: 0.25rem 0.5rem;
-  border-radius: 0.5rem;
-}
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import { useRouter } from "vue-router";
+import handleSignIn from "@/functions/auth/SignIn";
+import {ref} from "vue";
 
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
+const router = useRouter();
+
+const email = ref('');;
+const password = ref('');
+
+
+const goToRegister = () => {
+  router.push({ name: 'signup' });
+}; 
+
+const onSubmit = async () => {
+  const data = {
+      username: email.value,
+      password: password.value,
+  };
+    await handleSignIn(data);
+};
+
+</script>
+
+
+<style>
 </style>
