7e195c6343681689f425441b2bf74b816d90355d
selimo11 <s.oueslati.contact@gmail.com>
2025-04-17 18:05:38 +0200

    (feat) : configuring(forcing) light mode on views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container p-4">
-        <h1 class="text-2xl font-bold mb-4">Home</h1>
-        <p class="mb-4">Welcome to the home page!</p>
-    </div>
+  <div class="container p-4 bg-white text-black min-h-screen">
+    <h1 class="text-2xl font-bold mb-4">Home</h1>
+    <p class="mb-4">Welcome to the home page!</p>
+  </div>
 </template>
