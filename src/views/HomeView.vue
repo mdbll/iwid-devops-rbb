dd5a2a013eced5409335c15bad403831229a426e
bll <95914478+mdbll@users.noreply.github.com>
2025-04-17 17:32:55 +0200

    homepage + add signout button in navbar (#26)
    Co-authored-by: bll <iea.billy.kd@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,6 @@
-<template></template>
+<template>
+    <div class="container p-4">
+        <h1 class="text-2xl font-bold mb-4">Home</h1>
+        <p class="mb-4">Welcome to the home page!</p>
+    </div>
+</template>
