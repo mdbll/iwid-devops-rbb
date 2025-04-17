131f909c42cb06a553e7ab47eaaa45e2b8b4cb11
wared2003 <edouardn809@gmail.com>
2025-04-17 12:35:14 +0200

    delete nav

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/sign-up">Sign-up</RouterLink>
-        <RouterLink to="/sign-in">Sign-in</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
